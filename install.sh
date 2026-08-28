0929cb7064d26b38e3bd7164986af70fe15f2dd7
fhs2026 <longfahasa@gmail.com>
2026-08-28 15:23:30 +0700

    Update install script for fahasa-launcher package

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 set -e
 
 DEB_URL="https://github.com/fhs2026/huutu/raw/refs/heads/main/fahasa-launcher_1.0.1_all.deb"
-DEB_FILE="/tmp/fhs.deb"
+DEB_FILE="/tmp/fahasa-launcher_1.0.1_all.deb"
 
-echo "Đang tải fhs.deb..."
+echo "Đang tải fahasa-launcher_1.0.1_all.deb..."
 
 curl -L -o "$DEB_FILE" "$DEB_URL"
 
-echo "Đang cài đặt fhs.deb..."
+echo "Đang cài đặt fahasa-launcher_1.0.1_all.deb..."
 
 sudo apt install -y "$DEB_FILE"
 
