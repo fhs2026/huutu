f958c55d534437ab2755592c17d5697554c47ce5
fhs2026 <longfahasa@gmail.com>
2026-08-28 15:22:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
-Bash
+```bash
+#!/bin/bash
+
+set -e
+
+DEB_URL="https://github.com/fhs2026/huutu/raw/refs/heads/main/fahasa-launcher_1.0.1_all.deb"
+DEB_FILE="/tmp/fhs.deb"
+
+echo "Đang tải fhs.deb..."
+
+curl -L -o "$DEB_FILE" "$DEB_URL"
+
+echo "Đang cài đặt fhs.deb..."
+
+sudo apt install -y "$DEB_FILE"
+
+echo "Cài đặt hoàn tất!"
+```
