30cfda98e4fc5bad0f0cfa1fb37791b5782cd803
fhs2026 <longfahasa@gmail.com>
2026-08-28 15:24:52 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,13 +6,30 @@ set -e
 DEB_URL="https://github.com/fhs2026/huutu/raw/refs/heads/main/fahasa-launcher_1.0.1_all.deb"
 DEB_FILE="/tmp/fahasa-launcher_1.0.1_all.deb"
 
-echo "Đang tải fahasa-launcher_1.0.1_all.deb..."
+echo "================================"
+echo "       Cài đặt FHS"
+echo "================================"
 
-curl -L -o "$DEB_FILE" "$DEB_URL"
+if ! command -v curl >/dev/null 2>&1; then
+    echo "Đang cài curl..."
+    sudo apt update
+    sudo apt install -y curl
+fi
 
-echo "Đang cài đặt fahasa-launcher_1.0.1_all.deb..."
+echo "Đang tải fhs.deb..."
+curl -fL "$DEB_URL" -o "$DEB_FILE"
 
+if [ ! -s "$DEB_FILE" ]; then
+    echo "Lỗi: Không tải được fhs.deb."
+    exit 1
+fi
+
+echo "Đang cài đặt..."
 sudo apt install -y "$DEB_FILE"
 
-echo "Cài đặt hoàn tất!"
+rm -f "$DEB_FILE"
+
+echo "================================"
+echo "      Cài đặt thành công!"
+echo "================================"
 ```
